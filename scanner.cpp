b4e1a0490e206a006ed1726a4f96106f9dbae4b7
A7madRif3at <ahmedrefaa0809@gmail.com>
2026-05-05 01:15:31 +0300

    Add GeoIP lookup to scanner - country, city, ISP

--- scanner.cpp
+++ scanner.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main() {
     string ip;
     cout << "Enter target IP: ";
@@ -10,17 +12,28 @@ int main() {
 
     time_t now = time(0);
     string timestamp = ctime(&now);
-
+    // GeoIP lookup
+    string command = "curl -s http://ip-api.com/line/" + ip + "?fields=country,city,isp";
+    system(command.c_str());
+    
     // write to log 
     ofstream log("scan_log.txt");
     log << "=== Anubis Scan ===" <<endl;
     log << "Target: " << ip << endl;
     log << "Time: " << timestamp << endl;
+    log << "GeoIP Lookup: " << endl;
+    cout << "Scan logged for: " << ip << endl;
+
+    FILE* pipe = popen(command.c_str(), "r");
+    char buffer[256];
+    while(fgets(buffer, 256, pipe)) {
+        log << buffer;
+    }
+    pclose(pipe);
     log << "Status: Pending" << endl;
     log.close();
-
-    cout << "Scan logged for: " << ip << endl;
     system("pause");
+
     return 0;
 
 }
