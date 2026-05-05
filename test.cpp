35ffe616e05f0e6ba113f453ea5fb100059872dc
A7madRif3at <ahmedrefaa0809@gmail.com>
2026-05-06 00:04:56 +0300

    Test GeoIP lookup to scanner - country, city, ISP

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string ip;
+    cout << "Enter target IP: ";
+    cin >> ip;
+    cout << "Target IP: " << ip << endl;
+
+    time_t now = time(0);
+    string timestamp = ctime(&now);
+    string command = "curl -s http://ip-api.com/line/" + ip + "?fields=country,city,isp";
+    system(command.c_str());
+
+    ofstream log("scan_log.txt");
+    log << "=== Anubis Scan ===" << endl;
+    log << "Target: " << ip << endl;
+    log << "Time: " << timestamp << endl;
+    log << "GeoIP Lookup: " << endl;
+    cout << "Scan logged for: " << ip << endl;
+
+    FILE* pipe = popen(command.c_str(), "r");
+    char buffer[256];
+    while(fgets(buffer, 256, pipe)) {
+        log << buffer;
+    }
+    pclose(pipe);
+    log << "Statues: Pending" << endl;
+    log.close();
+    system("pause");
+
+    return 0;
+
+}
